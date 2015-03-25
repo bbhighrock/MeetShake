43a6765804a2f32be5e331ce3ed3aa1f517dd7d0
Ibrahim ABADA <ibraabada@gmail.com>
2015-03-25 16:43:59 +0100

    Clean projet

--- www/server.js
+++ www/server.js
@@ -3,6 +3,20 @@
 var express = require('express');
 var app = express();
 
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'meetshake'
+
+});
+ 
+
+
+
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -22,7 +36,15 @@ app.listen(8333);
 console.log('8333 is the magic port');
 
 
+connection.connect();
+connection.query('SELECT * FROM cards', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
 
+connection.end();
 /*
  register (enregistrement d'un utilisateur )
 
@@ -42,6 +64,5 @@ console.log('8333 is the magic port');
 	/mycards by id personne
 
  confPerimetre (Curseur périmétre de géolocalisation)
-
 
 */
