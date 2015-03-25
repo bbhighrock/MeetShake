5bec12f15f183088d59223309ee7c88d7ff839fa
Ibrahim ABADA <ibraabada@gmail.com>
2015-03-25 13:21:02 +0100

    Server.js fonctionne !

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
-var http = require("http");
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
 
-http.listen(8888);
-console.log('8080 is the magic port');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8333);
+console.log('8333 is the magic port');
+
+
+
+/*
+ register (enregistrement d'un utilisateur )
+
+ authentification (connection d'un utilisateur mdp / password)
+
+ cards ( Creation / Modification / Suppression / Select1: Geoloc ou|et Théme / Select2: Mes cards )
+
+	/addCard 
+		Insertion carte 
+
+	/majCard by id
+
+	/delete by id
+
+	/cards by geoloc et|ou theme
+
+	/mycards by id personne
+
+ confPerimetre (Curseur périmétre de géolocalisation)
+
+
+*/
